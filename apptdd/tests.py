6ecd83a14e26b3e2fa5fa1ddfd0f6e413b611d4f
MrSmailWhitle <504727360@qq.com>
2017-02-26 14:42:13 +0800

    resolve to know the rul.

--- apptdd/tests.py
+++ apptdd/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
-
+from django.core.urlresolvers import resolve
 # Create your tests here.
+class homepage(TestCase):
+    def test_resolveHomePage(self):
+        resolver=resolve('/')
