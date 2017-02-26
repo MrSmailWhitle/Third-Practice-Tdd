e1551d1defd2b1d6c8d24dae7c17123c00807aa2
MrSmailWhitle <504727360@qq.com>
2017-02-26 22:31:51 +0800

    the first migration for orm.

--- apptdd/tests.py
+++ apptdd/tests.py
@@ -3,6 +3,7 @@ from django.core.urlresolvers import resolve
 from apptdd.views import home
 from django.template.loader import render_to_string
 from django.http import HttpRequest
+from apptdd.models import Item
 # Create your tests here.
 class homepage(TestCase):
     #test the resolve is ok
@@ -33,3 +34,20 @@ class homepage(TestCase):
         excepted_html=render_to_string('home.html',{'new_item_text':'a new list item'})
         print(excepted_html)
         self.assertEqual(resp.content.decode(),excepted_html)
+class ItemModel(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item=Item()
+        first_item.text='The first list item'
+        first_item.save()
+
+        second_item=Item()
+        second_item.text='Item the second'
+        second_item.save()
+
+        saved_items=Item.objects.all()
+        self.assertEqual(saved_items.count(),2)
+
+        first_saved_item=saved_items[0]
+        second_saved_item=saved_items[1]
+        self.assertEqual(first_saved_item.text,'The first list item')
+        self.assertEqual(second_saved_item.text,'Item the second')
